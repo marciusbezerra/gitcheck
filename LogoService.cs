4de91dcda44109892e85579d6055e901c1463e65
Marcius Bezerra <marciusbezerra@outlook.com>
2023-06-16 16:27:38 -0300

    - app version info - showing no git dirs and files

--- LogoService.cs
+++ LogoService.cs
@@ -6,15 +6,17 @@ namespace GitCheck
 {
     public class LogoService
     {
-        public static void PrintLogo()
+        public static void PrintLogo(string version)
         {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(@"   ___ _ _       ___ _               _    ");
             Console.WriteLine(@"  / _ (_) |_    / __\ |__   ___  ___| | __");
             Console.WriteLine(@" / /_\/ | __|  / /  | '_ \ / _ \/ __| |/ /");
             Console.WriteLine(@"/ /_\\| | |_  / /___| | | |  __/ (__|   < ");
             Console.WriteLine(@"\____/|_|\__| \____/|_| |_|\___|\___|_|\_\");
             Console.WriteLine(@"       copyright © 2022 by Marcius Bezerra");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@$"       {version}");
             Console.ResetColor();
         }
     }
